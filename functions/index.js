f32fc535a69fc653935e45dc114dd2fd5c5042de
Christina Holland <hsubox@gmail.com>
2019-06-04 13:42:36 -0700

    Set up creation of screenname lookups

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,57 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
-admin.initializeApp();
+const request = require('request');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+admin.firestore().settings({ timestampsInSnapshots: true });
+
+function getTwitterUser(tid) {
+  return new Promise((resolve, reject) => {
+    const options = {
+      url: `https://api.twitter.com/1.1/users/show.json?user_id=${tid}`,
+      method: 'GET',
+      headers: {
+        "Authorization": `Bearer ${functions.config().twitter.token}`
+      }
+    };
+    function onResponse(err, res, body) {
+      if (err) {
+        reject(err);
+      } else {
+        try {
+          const json = JSON.parse(body);
+          resolve(json);
+        } catch(parseError) {
+          reject(parseError);
+        }
+      }
+    }
+    request(options, onResponse);
+  });
+}
+
 exports.onNewUser = functions.auth.user().onCreate(user => {
-  admin.firestore().collection('userTest').doc(user.uid).set({ tid: user.providerData[0].uid });
+  const tid = user.providerData[0].uid;
+  return getTwitterUser(user.providerData[0].uid)
+    .then((tUser) => {
+      const screenName = tUser.screen_name;
+      const userWrite = admin.firestore()
+        .collection('users')
+        .doc(user.uid)
+        .set({
+          tid,
+          screenName: screenName
+        });
+      const screenNameWrite = admin.firestore()
+        .collection('screenNames')
+        .doc(screenName)
+        .set({
+          uid: user.uid,
+          tid
+        });
+      return Promise.all([userWrite, screenNameWrite]);
+    })
+    .catch(e => {
+      console.error(e);
+    });
 });

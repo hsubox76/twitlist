4cae3fe1b20fabdf4c12913f2bb9789d1d3c15a6
Christina Holland <hsubox@gmail.com>
2019-10-02 18:17:24 -0700

    Embed firebase in bundle

--- crx/src/background.js
+++ crx/src/background.js
@@ -1,3 +1,6 @@
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
 import { firebaseConfig } from "../../shared/firebase-config";
 import { ACTION, COLL } from "../../shared/constants";
 
